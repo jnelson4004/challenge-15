65c695c3888c2237904e127bcfe7e6feea8ae4a1
burt reynolds <jnelson4004@gmail.com>
2024-06-06 11:12:48 -0500

    can't remember if i pushed last night

--- client/src-sw.js
+++ client/src-sw.js
@@ -32,4 +32,14 @@ offlineFallback({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
