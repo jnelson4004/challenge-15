a9425f3ffc4bb0d2123f2d2ae12136f9a2a31ccd
burt reynolds <jnelson4004@gmail.com>
2024-06-06 11:34:42 -0500

    this might work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,8 +48,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        
-      ],
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+      }],
     },
   };
 };
